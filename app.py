c5b772a9cab6c19139b7bc26b5fce562376d6068
Ram Jadhav <ramdj81@gmail.com>
2022-08-03 11:04:12 +0530

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Starting Machine Learning Project 2" 
+    return "CI CD pipeline has been established" 
 
 
 
